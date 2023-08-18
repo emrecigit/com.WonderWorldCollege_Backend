e85b3b4d0edb0e2d14e90f119be0d737499d7747
Emre Cigit <125315547+emrecigit@users.noreply.github.com>
2023-08-18 13:05:53 +0300

    first commit1

---
+++ src/test/java/hooks/HooksDB.java
@@ -0,0 +1,10 @@
+package hooks;
+
+import org.junit.After; //import io.cucumber.java.After;
+import org.junit.Before; // import io.cucumber.java.Before;
+import utilities.ConfigReader;
+
+import java.sql.*;
+
+public class HooksDB {
+}
